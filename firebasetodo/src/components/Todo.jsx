64320076c37a19100cbf219a0f9d456dd7b2113d
Michaela-K <michaelakodes@gmail.com>
2023-03-19 12:23:03 -0400

    add to Todo, icons and alignment

--- firebasetodo/src/components/Todo.jsx
+++ firebasetodo/src/components/Todo.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import {IoTrashOutline} from 'react-icons/io5'
+import {MdOutlineModeEditOutline} from 'react-icons/md'
 
-const Todo = () => {
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize rounded-md`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize rounded-md`,
+  row: `w-full flex justify-between`,
+  rowInner: `flex`,
+  rowInner2: `w-10 flex justify-between`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer flex items-center `,
+};
+
+const Todo = ({todos}) => {
+
   return (
-    <div>
-      item 1
-    </div>
+    <li className={style.li}>
+      <div className={style.row}>
+        <div className={style.rowInner}>
+          <input type="checkbox"/>
+          <p className={style.text}>{todos}</p>
+        </div>
+        <div className={style.rowInner2}>
+          <button>{<MdOutlineModeEditOutline  size={23} color="blue"/>}</button>
+          <button>{<IoTrashOutline size={23} color="crimson"/>}</button>
+        </div>
+      </div>
+    </li>
   )
 }
 
--- firebasetodo/src/components/TodoList.jsx
+++ firebasetodo/src/components/TodoList.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import {AiOutlinePlus} from 'react-icons/ai'
+import React, { useState } from 'react'
+import {IoIosAddCircleOutline} from 'react-icons/io'
 import Todo from './Todo'
 
 const style = {
   // bg: `h-screen w-screen p-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...`
   bg: `h-screen w-screen p-4 bg-gray-200 bg-[url("https://www.transparenttextures.com/patterns/purty-wood.png")]`,
-  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  container: `grid h-auto place-items-center bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 text-slate-100 bg-gradient-to-r from-green-300 to-blue-500 hover:from-yellow-400 hover:to-pink-500 ...`,
+  input: `border my-5 p-2 w-full text-xl rounded-md`,
+  button: `ml-2 hover:text-blue-400`,
   count: `text-center p-2`,
 }
 
 const TodoList = () => {
+  const [todos, setTodos] = useState(['study', 'clean']) 
   return (
     <div>
       <div className={style.container}>
-        <h3 className={style.heading}>Todo App</h3>
-        <form className={style.form}>
-          <input type="text" className={style.input} placeholder="Another thing to do"/>
-          <button className={style.button}>
-            <AiOutlinePlus size={30}/>
-          </button>
-        </form>
-        <ul>
-          <Todo/>
-        </ul>
+        <div>
+          <h3 className={style.heading}>To Do List</h3>
+          <p className={style.count}>{`You have ${todos.length} items left`}</p>
+          <form className={style.form}>
+            <input type="text" className={style.input} placeholder="Another thing to do"/>
+            <button className={style.button}>
+              {<IoIosAddCircleOutline size={40}/>}
+            </button>
+          </form>
+          <ul>
+          {todos.map((todos, index) =>(
+              <Todo key={index} todos={todos} />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
