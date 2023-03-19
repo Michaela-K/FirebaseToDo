e17362d33a22ac70e8008395e418b39c05b369f3
Michaela-K <michaelakodes@gmail.com>
2023-03-19 08:54:59 -0400

    move tailwind installation

--- firebasetodo/src/App.js
+++ firebasetodo/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./index.css";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...`
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className={style.bg}>
+       Hello
     </div>
   );
 }
